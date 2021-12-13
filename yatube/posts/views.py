120fa97c0a834b4816986125ce8f5b5a824e8810
Nickolay Evsin <nickolay.evsin@yandex.ru>
2021-12-14 02:02:37 +0300

    templates, models, urls, views was corrected

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -4,7 +4,7 @@ from django.core.paginator import Paginator
 from django.shortcuts import get_object_or_404, redirect, render
 
 from .forms import CommentForm, PostForm
-from .models import Comment, Follow, Group, Post, User
+from .models import Follow, Group, Post, User
 
 
 def pagination(request, objects):
